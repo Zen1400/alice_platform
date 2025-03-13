72d2aff4641bdf8bdf4053bcee1950e4c29c83f3
zein moualla <caizer1400@gmail.com>
2025-03-13 20:58:43 +0100

    working on Static serving on AWS, still not finished. Added the AWS CLI

--- learning_platform/apps/courses/models.py
+++ learning_platform/apps/courses/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
-    video = models.FileField(upload_to='course_videos/', blank=True, null=True)
+    image = models.ImageField(upload_to='media/course_images/', blank=True, null=True)
+    video = models.FileField(upload_to='media/course_videos/', blank=True, null=True)
     content = models.TextField(blank=True)  # This field can contain HTML or Markdown text
     code_snippet = models.TextField(blank=True)  # Optional field for code examples
     created_at = models.DateTimeField(auto_now_add=True)
