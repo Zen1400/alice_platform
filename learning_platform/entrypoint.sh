1d855a53ed9b755c85bfb3df231bed08a8fd64d6
zein moualla <caizer1400@gmail.com>
2025-03-16 11:59:55 +0100

    Finished Docker Compose part. Now the container is running correctly

--- learning_platform/entrypoint.sh
+++ learning_platform/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Wait for environment variables (if needed) and then:
 python manage.py collectstatic --noinput
-gunicorn learning_platform.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn learning_platform.wsgi:application --bind 0.0.0.0:8000 --workers 3
